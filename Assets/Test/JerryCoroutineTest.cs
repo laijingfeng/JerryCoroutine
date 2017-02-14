fe7292c26fbcd185aea6893f80b4cba03fff5117
laijingfeng <337292616@qq.com>
2017-02-14 11:54:11 +0800

    update

---
+++ Assets/Test/JerryCoroutineTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Jerry;
+
+public class JerryCoroutineTest : MonoBehaviour
+{
+    private CoroutineTask task1;
+
+    void Start()
+    {
+        task1 = new CoroutineTask(IE_Test1(), true);
+        task1.Finished += (manual) =>
+        {
+            Debug.LogWarning("IE_Test1 Finished " + manual);
+        };
+    }
+
+    void OnDestroy()
+    {
+        if (task1 != null)
+        {
+            task1.Stop();
+        }
+    }
+
+    private IEnumerator IE_Test1()
+    {
+        Debug.LogWarning("IE_Test1 1");
+        yield return Yielders.GetWaitForSeconds(1.0f);
+        Debug.LogWarning("IE_Test1 2");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# JerryCoroutine
-JerryCoroutine
+项目 | 内容
+---|---
+标题 | JerryCoroutine
+目录 | Unity/模块
+标签 | 协程
+备注 | 无
+更新 | 2017-02-14 11:22:56
+
+[TOC]
+
+## 说明
+
+依赖`Singleton.dll`
+
+## 摘自
+
+- [Unity-TaskManager](https://github.com/krockot/Unity-TaskManager)
+- [Yielders](https://github.com/PerfAssist/PA_Common/blob/master/Scripts/Yielders.cs)
